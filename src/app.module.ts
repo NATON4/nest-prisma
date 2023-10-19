720e7066aeddb78671a4cadb58286a98f9e40f8b
Anton Hryhorenko <a.o.hryhorenko.fitis21@chdtu.edu.ua>
2023-10-19 15:59:21 +0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {UserService} from './app.service';
+import {PostService} from './post.service';
+import {PrismaService} from './prisma.service';
+
+@Module({
+    imports: [],
+    controllers: [AppController],
+    providers: [UserService, PostService, PrismaService],
+})
+export class AppModule {
+}
